516a21b2a0910c3709ccc98a2693d7c1a868bfab
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-10-11 00:28:03 +0300

    destroy added with confirmation

--- redux-todo-app/src/components/TodoList.js
+++ redux-todo-app/src/components/TodoList.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from "react-redux";
-import { toggle } from "../redux/todos/todosSlice";
+import { toggle, destroy } from "../redux/todos/todosSlice";
 
 function TodoList() {
     const dispatch = useDispatch();
     const items = useSelector(state => state.todos.items);
-    console.log(items);
+    const handleDestroy = (id) => {
+        if (window.confirm("Are you sure?")) {
+            dispatch(destroy(id));
+        }
+    };
+
     return (
         <ul className="todo-list">
 			{/* <li className="completed">
@@ -27,7 +32,7 @@ function TodoList() {
                               onChange={() => dispatch(toggle({ id: item.id }))} 
                             />
                             <label>{item.title}</label>
-                            <button className="destroy"></button>
+                            <button className="destroy" onClick={() => handleDestroy(item.id)} ></button>
                         </div>
 			        </li>
                 ))
